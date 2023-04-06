7f284957e2420331eaceed02b0cd3744ea31b0da
ThaliaLevy <thalialevy@hotmail.com>
2023-04-05 23:08:43 -0300

    feat: arquivos de testes com perfil test

--- src/test/java/com/thalos/trailerflix/controllers/MovieControllerTest.java
+++ src/test/java/com/thalos/trailerflix/controllers/MovieControllerTest.java
@@ -1,18 +1,11 @@
 package com.thalos.trailerflix.controllers;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,6 +16,7 @@ import com.thalos.trailerflix.services.MovieService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class MovieControllerTest {
 
 	@Autowired
