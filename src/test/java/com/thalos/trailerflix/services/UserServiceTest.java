cc2bf36cbe3cbfaa57b3bca3436f3f4c6f5a7dbc
ThaliaLevy <thalialevy@hotmail.com>
2023-04-16 12:51:47 -0300

    test: ajuste

--- src/test/java/com/thalos/trailerflix/services/UserServiceTest.java
+++ src/test/java/com/thalos/trailerflix/services/UserServiceTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.thalos.trailerflix.exceptions.ObjectNotFoundException;
